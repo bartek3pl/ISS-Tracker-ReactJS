eb1c3bd7716843d289daf4430524ace8f9bc1992
bartek3pl <bartekh1998@op.pl>
2019-03-25 22:41:17 +0100

    Updates

---
+++ precache-manifest.ddf4f890c1f3054b635b55585d71b7dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b041a675a4d2005a226e829a566f5d6e",
+    "url": "/ISS-Tracker-ReactJS/static/media/iss.b041a675.png"
+  },
+  {
+    "revision": "5405546f795e7ea4549a7b43e0d60704",
+    "url": "/ISS-Tracker-ReactJS/static/media/earth.5405546f.png"
+  },
+  {
+    "revision": "3e76cb1da1684c9dfe25",
+    "url": "/ISS-Tracker-ReactJS/static/js/runtime~main.30a3ca94.js"
+  },
+  {
+    "revision": "c9e3ce5a441390646e60",
+    "url": "/ISS-Tracker-ReactJS/static/js/main.e224a382.chunk.js"
+  },
+  {
+    "revision": "a33238adbc15d0a18ae3",
+    "url": "/ISS-Tracker-ReactJS/static/js/2.b4fcef41.chunk.js"
+  },
+  {
+    "revision": "c9e3ce5a441390646e60",
+    "url": "/ISS-Tracker-ReactJS/static/css/main.c6fcf1f2.chunk.css"
+  },
+  {
+    "revision": "6e731c9ddd50b69a587285462c2ecd15",
+    "url": "/ISS-Tracker-ReactJS/index.html"
+  }
+];
